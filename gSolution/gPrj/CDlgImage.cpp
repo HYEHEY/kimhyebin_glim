53feb35c5cb4da55c1ea2e693fc2c7b92d569b2d
HYEHEY <althms107@gmail.com>
2023-09-21 11:44:17 +0900

    frame

---
+++ gSolution/gPrj/CDlgImage.cpp
@@ -0,0 +1,93 @@
+﻿// CDlgImage.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "gPrj.h"
+#include "afxdialogex.h"
+#include "CDlgImage.h"
+
+
+// CDlgImage 대화 상자
+
+IMPLEMENT_DYNAMIC(CDlgImage, CDialogEx)
+
+CDlgImage::CDlgImage(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_CDlgImage, pParent)
+{
+	m_pParent = pParent;
+}
+
+CDlgImage::~CDlgImage()
+{
+}
+
+void CDlgImage::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(CDlgImage, CDialogEx)
+	ON_WM_PAINT()
+END_MESSAGE_MAP()
+
+
+// CDlgImage 메시지 처리기
+
+
+BOOL CDlgImage::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	InitImage();
+
+	return TRUE;  // return TRUE unless you set the focus to a control
+	// 예외: OCX 속성 페이지는 FALSE를 반환해야 합니다.
+}
+
+void CDlgImage::InitImage()
+{
+	// 이미지 생성
+	int nWidth = 640;
+	int nHeight = 480;
+	int nBpp = 8;
+
+	m_image.Create(nWidth, -nHeight, nBpp);
+	if (nBpp == 8) {
+		static RGBQUAD rgb[256];
+		for (int i = 0; i < 256; i++) {
+			rgb[i].rgbRed = rgb[i].rgbGreen = rgb[i].rgbBlue = i;
+			m_image.SetColorTable(0, 256, rgb);
+		}
+	}
+
+	int nPitch = m_image.GetPitch();
+	unsigned char* fm = (unsigned char*)m_image.GetBits();
+
+	memset(fm, 0xff, nWidth * nHeight);
+}
+
+void CDlgImage::OnPaint()
+{
+	CPaintDC dc(this); // device context for painting
+	// 그리기 메시지에 대해서는 CDialogEx::OnPaint()을(를) 호출하지 마십시오.
+	// 화면에 출력
+	
+
+
+	// 결과 그리기->원그리기
+	drawData(&dc);
+}
+
+void CDlgImage::drawData(CDC* pDC)
+{
+	int nRadius = 0;
+	int CenterX = m_image.GetWidth();
+	int CenterY = m_image.GetHeight();
+	int nSumX = rand() % (CenterX - nRadius * 2) +nRadius;
+	int nSumY = rand() % (CenterY - nRadius * 2) + nRadius;
+	
+	//CRect rect(nSumX, nSumY, 100, 100);
+	CRect rect(nSumX, nSumX, nSumY, nSumY);
+	pDC->Ellipse(rect);
+}
---
+++ gSolution/gPrj/CDlgImage.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+#include "afxdialogex.h"
+
+#define MAX_POINT	1000		// 상수 정의
+// CDlgImage 대화 상자
+
+class CDlgImage : public CDialogEx
+{
+	DECLARE_DYNAMIC(CDlgImage)
+
+public:
+	CDlgImage(CWnd* pParent = nullptr);   // 표준 생성자입니다.
+	virtual ~CDlgImage();
+
+	CImage m_image;
+	CWnd* m_pParent;
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_CDlgImage };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+	DECLARE_MESSAGE_MAP()
+public:
+	virtual BOOL OnInitDialog();
+
+private:
+	void InitImage();
+	void drawData(&dc);
+public:
+	afx_msg void OnPaint();
+};
